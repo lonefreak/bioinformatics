7968bc8270f3446f2b6567e034f1cdf8e53476d5
Fabricio <Fabricio@mendel.(none)>
2013-03-29 12:53:32 -0300

    Adicionando biblioteca de combinatoria

--- hth99sm2/group_otus_by_sample.pl
+++ hth99sm2/group_otus_by_sample.pl
@@ -1,30 +1,46 @@
 #! /usr/bin/perl
 
 use strict;
+use Algorithm::Combinatorics qw(combinations);
 
 my $otu_table = shift;
 my %samples;
+my $caption_sum = 0;
+my $line_sum = 0;
+my $total_lines = 0;
+my @caption;
 
 foreach my $arg (@ARGV) {
 	$samples{$arg} = 1;
 }
-&print_hash(\%samples);
+#&print_hash(\%samples);
 
 open(TABLE,"<$otu_table") || die "Could not open the file $otu_table\n";
 <TABLE>;
 while(<TABLE>) {
 	chomp;
-	#print "$_\n";
 	if($_ =~ m/^\#OTU/) {
 		&process_caption($_); #proccess caption
+		print "Procurando por somas iguais a $caption_sum\n\n";
+		print $_, "\n";
 	} elsif($_ =~ m/^\#/) {
-		#discard this line
+		next; #discard this line
 	} else{
-		#process line according to args
+		&process_line($_);
+		if($caption_sum == $line_sum) {
+			#print $_, "\n";
+			$total_lines++;
+		}
+		$line_sum = 0;
 	}
 }
+close(TABLE);
 
 &print_hash(\%samples);
+
+#print "Procurando por somas iguais a $caption_sum\n\n";
+
+print "Total de linhas encontradas: $total_lines\n\n";
 
 sub print_hash {
         my (%hash) = %{$_[0]};
@@ -34,10 +50,25 @@ sub print_hash {
 }
 
 sub process_caption {
-	my @caption = split("\t", $_[0]);
-	for(my $i = 1; $i < $#caption; $i++) {
+	@caption = split("\t", $_[0]);
+	for(my $i = 1; $i <= $#caption; $i++) {
 		if(defined($samples{$caption[$i]})) {
 			$samples{$caption[$i]} = 2**$i;
+			$caption_sum += 2**$i;
+			print "################################";
 		}
+		print $caption[$i], " => ", $i, "\n";
+	}
+}
+
+sub process_line {
+	my @line = split("\t", $_[0]);
+	for(my $i = 1; $i < $#line; $i++) {
+		if($line[$i] + 0.0 > 0) {
+			#print $caption[$i],"\n";
+			#print $line[$i], " + 0.0 = ", ($line[$i] + 0.0), "\n"; 
+			$line_sum += 2**$i;
+		}
+		#print "LINESUM: ",$line_sum,"\n";
 	}
 }
