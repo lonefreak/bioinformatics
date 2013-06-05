a7ff9ab1e08925a0b31843924d833e3001e34117
Fabricio Leotti <fabricio.leotti@abril.com.br>
2013-06-04 22:27:25 -0300

    Adicionando sub pra calcular N50

--- average_length.pl
+++ average_length.pl
@@ -3,11 +3,11 @@
 #                                                        
 #  PROGRAM: average_length.pl                                     24.Oct.2012     
 #
-#  DESCRIPTION: Calculates the average, maximum and minimum size of sequences in a fasta file       
+#  DESCRIPTION: Calculates the average, N50, maximum and minimum size of sequences in a fasta file       
 #
 #  AUTHOR: Fabricio Leotti
 #
-#  LAST MODIFIED: 30.Mai.2013
+#  LAST MODIFIED: 04.Jun.2013
 #  
 use strict;
 use warnings;
@@ -20,6 +20,7 @@ if (!defined($ARGV[0])) {
 
 my $file = $ARGV[0];
 my $nbr_sequences = my $seq_length = my $average_length = my $total_seq_length = my $max_length = my $min_length = 0;
+my @n50values;
 open(FILE, "<$file") || die "cannot open $file\n";
 while(<FILE>){
 	chomp;
@@ -27,13 +28,16 @@ while(<FILE>){
 		$total_seq_length += $seq_length;
 		if($seq_length > $max_length) { $max_length = $seq_length; }
 		if($seq_length < $min_length || $min_length == 0) { $min_length = $seq_length; }
+		if($seq_length) {
+			push(@n50values, $seq_length);
+		}
 		$seq_length = 0; 
 		$nbr_sequences++;
 	} else {
 		$seq_length += length($_);
 	}
 }
-
+push(@n50values, $seq_length);
 $total_seq_length += $seq_length;
 if($seq_length > $max_length) { $max_length = $seq_length; }
 if($seq_length < $min_length || $min_length == 0) { $min_length = $seq_length; }
@@ -44,4 +48,19 @@ $average_length = $total_seq_length / $nbr_sequences;
 print "Total number of sequences: $nbr_sequences\n";
 print "Average:\t$average_length\n";
 print "Max:\t$max_length\n";
-print "Min:\t$min_length\n\n";
+print "Min:\t$min_length\n";
+my $n50 = &n50($total_seq_length, \@n50values);
+print "N50:\t$n50\n\n";
+
+sub n50 {
+	my @arr = @{$_[1]};
+	my $total_length = $_[0];
+	@arr = reverse sort {$a <=> $b} @arr;
+	my $n50 = $arr[0];
+	my $covered = $total_length;
+	while ($covered > $total_length/2) {
+	        $n50 = shift(@arr);
+	        $covered -= $n50;
+	}
+	return $n50;
+}
